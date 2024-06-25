230cd3d55cda5dee929e0f86e24e30560bd54c83
kikodimov2002@gmail.com <kikodimov2002@gmail.com>
2024-06-25 18:11:51 +0300

    moved to usercontroller

--- server/controllers/ContactController.js
+++ server/controllers/ContactController.js
@@ -1,5 +1,4 @@
 let contacts = require("../data/contacts.json");
-let bcrypt = require('bcryptjs');
 
 const { sortContacts } = require("../utils/utilities");
 
@@ -94,48 +93,6 @@ function getFavoriteContacts(req, res) {
   res.status(200).json(contacts.filter((contact) => contact.isFavorite));
 }
 
-function registerUser(req, res) {
-  const {email, password, salt} = req.body;
-
-  if ([email, password, salt].includes(undefined))
-  {
-    res.status(400).send(`Missing email, password or salt`);
-    return;
-  }
-
-  bcrypt.hash(password, 8, function(err, hash) {
-    // TODO: store hash in DB
-  });
-
-  res.status(200).send(`Registered ${email}`);
-}
-
-function loginUser(req, res) {
-  const {email, password} = req.body;
-
-  if ([email, password].includes(undefined))
-  {
-    res.status(400).send(`Missing email, password or salt`);
-    return;
-  }
-
-  let dbPassword = "TODO"; // TODO: DB
-  bcrypt.compare(dbPassword, password).then((result) => {
-    // result := true | false
-    // TODO
-    if (result)
-    {
-      res.status(200).send(`Logged in ${email}`);
-      return;
-    }
-    else
-    {
-      res.status(400).send(`Wrong password for ${email}`);
-    }
-  });
-}
-
-
 module.exports = {
   getContact,
   createContact,
@@ -143,6 +100,4 @@ module.exports = {
   deleteContact,
   getAllContacts,
   getFavoriteContacts,
-  registerUser,
-  loginUser,
 };
---
+++ server/controllers/UserController.js
@@ -0,0 +1,49 @@
+let bcrypt = require('bcryptjs');
+
+function registerUser(req, res) {
+    const {email, password, salt} = req.body;
+
+    if ([email, password, salt].includes(undefined))
+    {
+      res.status(400).send(`Missing email, password or salt`);
+      return;
+    }
+
+    bcrypt.hash(password, 8, function(err, hash) {
+      // TODO: store hash in DB
+    });
+
+    res.status(200).send(`Registered ${email}`);
+}
+  
+function loginUser(req, res) {
+    const {email, password} = req.body; 
+    if ([email, password].includes(undefined))
+    {
+      res.status(400).send(`Missing email, password or salt`);
+      return;
+    }   
+
+    let dbPassword = "TODO"; // TODO: DB
+
+    bcrypt.compare(dbPassword, password).then((result) => {
+      // result := true | false
+      // TODO
+      if (result)
+      {
+        res.status(200).send(`Logged in ${email}`);
+        return;
+      }
+      else
+      {
+        res.status(400).send(`Wrong password for ${email}`);
+      }
+    });
+}
+  
+
+module.exports = {
+    registerUser,
+    loginUser
+};
+  
